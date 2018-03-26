58f1cf0d061085ba22b13b71dc231c6215ee67c9
Bu Kinoshita <bukinoshita@gmail.com>
2018-03-26 19:46:08 -0300

    not a promise

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,20 +3,22 @@
 import test from 'ava'
 import rndDate from './..'
 
-test('should return a random date when passing one argument', async t => {
-  const rnd = await rndDate(2010).then(res => res)
+test('should return a random date when passing one argument', t => {
+  const rnd = rndDate(2010)
 
-  t.true(rnd.getFullYear() > 2009 && rnd.getFullYear() < new Date().getFullYear() + 1)
+  t.true(
+    rnd.getFullYear() > 2009 && rnd.getFullYear() < new Date().getFullYear() + 1
+  )
 })
 
-test('should return a random date when passing two arguments', async t => {
-  const rnd = await rndDate(2010, 2016).then(res => res)
+test('should return a random date when passing two arguments', t => {
+  const rnd = rndDate(2010, 2016)
 
   t.true(rnd.getFullYear() > 2009 && rnd.getFullYear() < 2017)
 })
 
-test('should return an error', async t => {
-  const rnd = await rndDate(2010, 2009).catch(err => err)
+test('should return an error', t => {
+  const error = rndDate(2010, 2009)
 
-  t.is(rnd, 'Invalid date. Start date should be higher than end date')
+  t.is(error.message, 'Invalid date. Start date should be higher than end date')
 })
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict'
 
 const rndDate = (from, to = new Date()) => {
-  return new Promise((resolve, reject) => {
-    let end
+  let end
 
-    if (from > to) {
-      reject(`Invalid date. Start date should be higher than end date`)
-    }
+  if (from > to) {
+    return new TypeError(
+      'Invalid date. Start date should be higher than end date'
+    )
+  }
 
-    to instanceof Date ? end = to : end = new Date(to, 0, 1)
-    const startDate = new Date(from, 0, 1)
-    const randomDate = new Date(startDate.getTime() + Math.random() * (end.getTime() - startDate.getTime()))
+  to instanceof Date ? (end = to) : (end = new Date(to, 0, 1))
+  const startDate = new Date(from, 0, 1)
+  const randomDate = new Date(
+    startDate.getTime() + Math.random() * (end.getTime() - startDate.getTime())
+  )
 
-    resolve(randomDate)
-  })
+  return randomDate
 }
 
 module.exports = rndDate
